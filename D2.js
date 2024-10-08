22c0d3d50d542b8db10f167377abac50b0033d8d
Martino Brundu <martino.brundu@libero.it>
2024-10-09 00:50:12 +0200

    finish

--- D2.js
+++ D2.js
@@ -3,12 +3,12 @@
 */
 let num1 = 25
 let num2 = 30
-let biggest
+let bigger
 
 if ( num1 > num2 ) 
-   { biggest = num1 } 
-else {biggest =  num2}
-console.log (biggest)
+   { bigger = num1 } 
+else {bigger =  num2}
+console.log (bigger)
 
 
 /* ESERCIZIO 2
@@ -18,7 +18,7 @@ console.log (biggest)
 
 let num4 = 7
 
-if (5 !== num4) { console.log ('not equal')
+if (num4 !== 5) { console.log ('not equal')
 
 } else {console.log('equal')}
 
@@ -41,7 +41,7 @@ else {console.log('divisibile per 5') }
 let num5 = 4
 let num6 = 12
 
-if ( num5 === 8 || num6 ===6 || num6 - num5 === 8 ) {console.log ('Verificato!')}
+if ( num5 === 8 || num6 === 8 || num6 - num5 === 8 || num6 + num5 === 8  || num5  - num6 === 8) {console.log ('Verificato!')}
 
 
 
@@ -62,7 +62,7 @@ let shippingCost = 10
 
 if ( totalShoppingCart > 50) {shippingCost=0}
 else {shippingCost} 
-let totale1 = (totalShoppingCart + shippingCost )
+ let totale1 = (totalShoppingCart + shippingCost )
 console.log(totale1)
 
 /* ESERCIZIO 6
@@ -70,53 +70,86 @@ console.log(totale1)
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
-
 let num10 = 18
 let num11 = 20
 let num12 = 15
-let totalShoppingCart1 = (num7+num8+num9)
-let shippingCost1 = 10
+let totalShoppingCart2 = (num10+num11+num12)
+let spedizione1 = 10
 
 
-if ( totalShoppingCart1 > 50) {shippingCost1=0}
-else {shippingCost} 
-let totale2 = (totalShoppingCart + shippingCost )
-const finaltotal1 = (totale2 / 100 ) * 20
-console.log(finaltotal1)
+let totale3 = ( totalShoppingCart2 - ( (totalShoppingCart2 / 100) *20) )
+if (totale3 > 50) {spedizione1 = 0 }
+console.log (totale3 + spedizione1)
+
+
+
+
+
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
   Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
   Alla fine mostra il risultato in console.
-*/
+// */
+// let1 = 70
+// let2 = 20
+// let3 = 30
+// if (let1 > let2) 
+// 
+// 
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x1= 10
+let x2= 20
+let x3= 30
+
+if (x1 < x2) 
+  {if (x1 < x3)
+    {if(x2 < x3)
+    {console.log(x1 , x2 , x3)}else{console.log(x1 , x3 , x2) } } else{console.log(x3, x1, x2)} } 
+else {if(x1<x3) {console.log(x1 , x2 , x3)} else{if(x2<x3) {console.log(x2, x3 , x1) } else{console.log(x3, x2,x1)} } }
+
+
+
+
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let numberX = "ciaone"
+console.log(typeof numberX)
+
+
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let number16 = 23432523
+
+if ( number16 % 2 === 0) {console.log('numero pari')}
+else {console.log('numero dispari')}
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
   let val = 7
   if (val < 10) {
-      console.log("Meno di 10");
-    } else if (val < 5) {
-      console.log("Meno di 5");
-    } else {
-      console.log("Uguale a 10 o maggiore");
+  console.log("Meno di 10");
+} else if (val < 5) {
+   console.log("Meno di 5"); } else {
+     console.log("Uguale a 10 o maggiore");
     }
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// let val = 7 
+// if (val < 10) {console.log('meno di 10')}
+// else if (val < 5) {console.log(' Meno di 5')}
+// else {console.log('Uguale a 10 o maggiore ')}
+
+let val = 15 
+ if (val < 5) {console.log(' Meno di 5')}
+else if  (val < 10) {console.log('meno di 10')}
+else {console.log('Uguale a 10 o maggiore ')}
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -126,30 +159,45 @@ const me = {
   name: 'John',
   lastName: 'Doe',
   skills: ['javascript', 'html', 'css'],
+  
 }
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+me.city = 'New York'
+
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+ delete me.lastName
+
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+ delete me.skills[2]
+ console.log(me)
 
-/* ESERCIZIO 14
+ /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+const firstarray = []
+
+firstarray.push (1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 )
+
+console.log(firstarray)
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+firstarray[9 ] = 100
+console.log(firstarray)
+
+
+
